d8cabc4efafb73b88b84d6eb7bcc317e4ab828fe
EC2 Default User <ec2-user@ip-172-31-20-230.us-west-2.compute.internal>
2014-10-15 21:54:51 +0000

    fix 3

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ var HTTP = require("http");
 var APP = require("express")();
 var BODY_PARSER = require('body-parser');
 
-<% for (var i = 0; i < connections.length; ++i) { %>
-<%   var conn = connections[i]; %>
+<% for (var i = 0; i < connections.length; ++i) {
+     var conn = connections[i]; %>
 
 app.<%= conn.method %>("<%= conn.path %>", function(req, res) {
   var sanitized = {};
 
-<%   for (var j = 0; conn.params && j < conn.params.length; ++j) { %> 
-<%     var name = conn.params[j].name; %>
-  var <%= name %> = req.body.<%= name %>;
-  if (!<%= name %>.match(conn.params[j].regex)) {
+<%   for (var j = 0; conn.params && j < conn.params.length; ++j) { 
+       var name = conn.params[j].name;
+       var regex = conn.params[j].regex; %>
+  var <%= name %> = req.body.<%= name %><%= regex ? '' : ' // Unsanitized' %>;
+<%     if (regex) { %>
+  if (!<%= name %>.match(<%= regex %>)) {
     console.log('bad input');
     return;
   } else {
     sanitized.<%= name %> = <%= name %>;
   }
-
+<%     } else { %>
+  sanitized.<%= name %> = <%= name %>;
+<%     } %>
 <%   } %>
 
   <%= conn.call %>(sanitized); 
