dccd1241b2958f2e4bbc6e9a21f14b8658d97dd9
EC2 Default User <ec2-user@ip-172-31-20-230.us-west-2.compute.internal>
2014-10-15 21:41:57 +0000

    fix 2

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ var BODY_PARSER = require('body-parser');
 
 <% for (var i = 0; i < connections.length; ++i) { %>
 <%   var conn = connections[i]; %>
+
 app.<%= conn.method %>("<%= conn.path %>", function(req, res) {
   var sanitized = {};
+
 <%   for (var j = 0; conn.params && j < conn.params.length; ++j) { %> 
-       var <%= conn.params.name %> = req.body.<%= conn.params.name %>;
-       if (!<%= conn.params.name %>.match(conn.params.regex)) {
-         console.log('bad input');
-         return;
-       } else {
-         sanitized.<%= conn.params.name %> = <%= conn.params.name %>;
-       }
+<%     var name = conn.params[j].name; %>
+  var <%= name %> = req.body.<%= name %>;
+  if (!<%= name %>.match(conn.params[j].regex)) {
+    console.log('bad input');
+    return;
+  } else {
+    sanitized.<%= name %> = <%= name %>;
+  }
+
 <%   } %>
+
   <%= conn.call %>(sanitized); 
 });
+app.listen(<%= port %>);
+
 <% } %>
