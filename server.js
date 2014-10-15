fb79cc229d44a237b981673c6da74ec1b96dc371
EC2 Default User <ec2-user@ip-172-31-20-230.us-west-2.compute.internal>
2014-10-15 21:19:54 +0000

    Initial template

---
+++ server.js
@@ -0,0 +1,20 @@
+var HTTP = require("http");
+var APP = require("express")();
+var BODY_PARSER = require('body-parser');
+
+<% for (var i = 0; i < connections.length; ++i) { %>
+<%   var conn = connections[i]; %>
+app.<%= conn.method %>("<%= conn.path %>", function(req, res) {
+  var sanitized = {};
+<%   for (var j = 0; j < conn.params.length; ++j) { %> 
+       var <%= conn.params.name %> = req.body.<%= conn.params.name %>;
+       if (!<%= conn.params.name %>.match(conn.params.regex)) {
+         console.log('bad input');
+         return;
+       } else {
+         sanitized.<%= conn.params.name %> = <%= conn.params.name %>;
+       }
+<%   } %>
+  <%= conn.call %>(sanitized); 
+});
+<% } %>
