be9b9b9f0d8d0ff415551310affd6f23aaed1c95
EC2 Default User <ec2-user@ip-172-31-20-230.us-west-2.compute.internal>
2014-10-15 22:07:39 +0000

    newline slurping

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ var APP = require("express")();
 var BODY_PARSER = require('body-parser');
 
 <% for (var i = 0; i < connections.length; ++i) {
-     var conn = connections[i]; %>
-app.<%- conn.method %>("<%- conn.path %>", function(req, res) {
+     var conn = connections[i]; -%>
+app.<%- conn.method -%>("<%- conn.path -%>", function(req, res) {
   var sanitized = {};
 <%   for (var j = 0; conn.params && j < conn.params.length; ++j) { 
        var name = conn.params[j].name;
-       var regex = conn.params[j].regex; %>
-  var <%- name %> = req.body.<%- name %><%- regex ? '' : ' // Unsanitized' %>;
-<%     if (regex) { %>
-  if (!<%- name %>.match("<%- regex %>")) {
+       var regex = conn.params[j].regex; -%>
+  var <%- name -%> = req.body.<%- name -%><%- regex ? '' : ' // Unsanitized' -%>;
+<%     if (regex) { -%>
+  if (!<%- name -%>.match("<%- regex -%>")) {
     console.log('bad input');
     return;
   } else {
-    sanitized.<%- name %> = <%- name %>;
+    sanitized.<%- name -%> = <%- name -%>;
   }
-<%     } else { %>
-  sanitized.<%- name %> = <%- name %>;
-<%     } %>
-<%   } %>
-  <%- conn.call %>(sanitized); 
+<%     } else { -%>
+  sanitized.<%- name -%> = <%- name -%>;
+<%     } -%>
+<%   } -%>
+  <%- conn.call -%>(sanitized); 
 });
-<% } %>
-app.listen(<%- port %>);
+<% } -%>
+app.listen(<%- port -%>);
